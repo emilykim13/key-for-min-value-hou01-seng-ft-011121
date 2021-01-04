fc1745932925f6aa2718093ce046cef06720c2b5
Emily Nguyen <emilyn0614@gmail.com>
2021-01-04 09:55:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,20 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  minimum_val = nil 
+  min_val = nil 
   key_min_val = nil
+  # current values?
+  # yah
+  name_hash.each do |key, value|
+    if min_val == nil 
+      min_val = value
+      key_min_val = key 
+    else 
+      if min_val > value
+        min_val = value
+        key_min_val = key 
+      end
+    end
+end
+min_key_val
 end
